8dd57b09936fa00c7e533dc1590e4d429ab571cf
Walker Dick <walkerraydick2@gmail.com>
2022-07-15 16:09:52 -0500

    New files

---
+++ main.py
@@ -0,0 +1,21 @@
+import serial # Import Serial Library
+import time # Import Time Library
+import yaml
+from yaml.loader import SafeLoader
+with open('config.yaml') as config_file:
+    data = yaml.load(config_file, Loader=SafeLoader)
+    print(data)
+print("Starting")
+connected = False
+COMPortName = "COM4" # Change this to your COM port name
+print("Trying to open + " + COMPortName)
+ser = serial.Serial(COMPortName, 1440, timeout=1)
+print("Connected")
+print("Opened COM4")
+print("Entering main loop")
+ser.write(b'SYN\n')
+while True:
+    try:
+        readOut = ser.readline().decode('ascii')
+    except:
+        pass
