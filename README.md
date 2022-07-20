98c56fe4590a31ec101b29ee6df9bcec24bb802c
Walker Dick <walkerraydick2@gmail.com>
2022-07-20 08:50:55 -0500

    Edit the python script to change the behavior of the program add new config to the example yaml file
    """

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Python-Scanner-Watch
+pushover library used => https://github.com/Wyattjoh/pushover
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import serial # Import Serial Library
+from pushover import Pushover # Import Pushover Library
 import time # Import Time Library
 import yaml
 from yaml.loader import SafeLoader
 with open('config.yaml') as config_file:
     data = yaml.load(config_file, Loader=SafeLoader)
     print(data)
+
+
+
+
+""" 
 print("Starting")
 connected = False
 COMPortName = "COM4" # Change this to your COM port name
@@ -18,4 +24,5 @@ while True:
     try:
         readOut = ser.readline().decode('ascii')
     except:
-        pass
+        pass 
+"""
